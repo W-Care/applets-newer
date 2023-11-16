4de26ba1f15a3812517ae9be4f2a7a68a9045c8e
newer <newer@qq.com>
2023-11-16 20:10:02 +0800

    实现分享和收藏以及视频播放

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -1,6 +1,6 @@
 // pages/post-detail/post-detail.js
 import {postList} from '../../data/data.js'
-const app=getApp()
+const app = getApp()
 Page({
 
   /**
@@ -22,10 +22,71 @@ Page({
     const postData=postList[options.pid]
     console.log(options)
     this.data._pid=options.pid
+    const postsCollected = wx.getStorageSync('posts_collected')
+    if(postsCollected){
+      this.data._postsCollected=postsCollected
+    }
+    let colleted = postsCollected[this.data._pid]
+    if(colleted=== undefined){
+      colleted = false
+    }
     this.setData({
-      postData
+      postData,
+      colleted,
+      isPlaying:this.currentMusicIsPlaying()
     })
+    const mgr=wx.getBackgroundAudioManager()
+    this.data._mgr=mgr
+    mgr.onPlay(this.onMusicStart)
+    mgr.onPause(this.onMusicStop)
     console.log(this.data)
+  },
+  currentMusicIsPlaying(){
+    if(app.gIsPlayingMusic &&app.gIsPlayingPostId ===this.data._pid){
+      return true
+    }
+    return false
+    
+  },
+  onMusicStart(event){
+    const mgr =this.data._mgr
+    const  music = postList[this.data._pid].music
+    mgr.src=music.url
+    mgr.title = music.title
+    mgr.coverImgUrl =music.coverImg
+    app.gIsPlayingMusic = true
+    app.gIsPlayPostId=this.data._pid
+    this.setData(
+      {isPlaying:true}
+    )
+  },
+  onMusicStop(event){
+    const mgr =this.data._mgr
+    mgr.pause()
+    app.gIsPlayingMusic=false
+    app.gIsPlayingPostId = -1
+    this.setData({
+      isPlaying:false
+    })
+  },
+  onCollect(event){
+    const postsCollected =this.data._postsCollected
+    wx.getStorageSync('key')
+    postsCollected[this.data._pid] = !this.data.collected
+    this.setData({
+      collected:!this.data.collected
+    })
+    wx.setStorageSync('posts_collected', postsCollected)
+    wx.showToast({
+      title: this.data.collected?'Succed':'Failed',
+      duration:3000
+    })
+  },
+  async onShare(event){
+    const result = await wx.showActionSheet({
+      itemList: ['分享到QQ','分享到微信','分享到朋友圈'],
+    })
+    console.log(result)
   },
 
   /**
