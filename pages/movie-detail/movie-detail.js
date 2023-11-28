97276a21ee5cb885ecf6b698e3dee73815d38e5a
newer <newer@qq.com>
2023-11-28 20:24:24 +0800

    电影详细页

--- pages/movie-detail/movie-detail.js
+++ pages/movie-detail/movie-detail.js
@@ -1,4 +1,5 @@
 // pages/movie-detail/movie-detail.js
+import {convertToCastString, convertToCastInfos} from '../../utils/util.js'
 const app = getApp()
 Page({
 
@@ -27,16 +28,27 @@ Page({
   },
   processMovieData(movie){
     const data ={}
+    data.directors = convertToCastString(movie.directors)
+    data.casts= convertToCastString(movie.casts)
     data.image=movie.images.large
     data.title =movie.title
     data.subtitle = movie.countries[0]+'.' +movie.year
     data.wishCount=movie.wish_count
     data.commentsCount =movie.comments_count
+    data.rating=movie.rating.stars/10
+    data.average = movie.rating.average
+    data.genres=movie.genres.join('、')
+    data.summary = movie.summary
+    data.castsInfo = convertToCastInfos(movie.casts)
     this.setData({
       movie:data
     })
   },
-
+  onViewPost(event){
+    wx.previewImage({
+      urls: [this.data.movie.image],
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
